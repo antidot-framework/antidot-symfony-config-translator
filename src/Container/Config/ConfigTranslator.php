4133a1b5030e479e31543cbbad06fabc07015ebe
kpicaza <kpicaza.sound@gmail.com>
2019-04-13 20:33:54 +0200

    add conditions to config translation

--- src/Container/Config/ConfigTranslator.php
+++ src/Container/Config/ConfigTranslator.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Antidot\SymfonyConfigTranslator\Container\Config;
 
+use DateTimeImmutable;
+
+use function array_key_exists;
+use function array_merge_recursive;
+use function array_search;
+use function json_decode;
+use function json_encode;
+use function str_replace;
+
 class ConfigTranslator
 {
     private const CONFIG = [
@@ -18,14 +27,26 @@ class ConfigTranslator
 
     public function __invoke(array $config): array
     {
-        $config = \array_replace_recursive($config, $config['parameters'] ?? []);
-        unset($config['parameters']);
+        if (false === empty($config['parameters'])) {
+            $config['parameters'] = json_decode(
+                str_replace(
+                    '%date%',
+                    (new DateTimeImmutable())->format('Y-m-d'),
+                    json_encode($config['parameters']) ?: ''
+                ),
+                true
+            );
+            $config = array_merge_recursive($config, $config['parameters'] ?? []);
+            unset($config['parameters']);
+        }
 
-        $config = \array_replace_recursive(
-            $config,
-            $this->parse($config)
-        );
-        unset($config['services']);
+        if (false === empty($config['services'])) {
+            $config = array_merge_recursive(
+                $config,
+                $this->parse($config)
+            );
+            unset($config['services']);
+        }
 
         return $config;
     }
@@ -39,17 +60,17 @@ class ConfigTranslator
                 continue;
             }
 
-            if (\array_key_exists('alias', $service)) {
-                $config['dependencies']['aliases'][$serviceName] = \str_replace('@', '', $service['alias']);
+            if (array_key_exists('alias', $service)) {
+                $config['dependencies']['aliases'][$serviceName] = str_replace('@', '', $service['alias']);
                 continue;
             }
 
-            if (\array_key_exists('factory', $service)) {
+            if (array_key_exists('factory', $service)) {
                 $config['dependencies']['factories'][$serviceName] = $service['factory'][0];
                 continue;
             }
 
-            if (\array_key_exists('arguments', $service)) {
+            if (array_key_exists('arguments', $service)) {
                 $arguments = [];
                 /**
                  * @var string $argument
@@ -59,13 +80,13 @@ class ConfigTranslator
                     $isService = 0 === \strpos('@', $value);
 
                     if ($isService) {
-                        $arguments[\str_replace('$', '', $argument)] = $this->searchDependency(
+                        $arguments[str_replace('$', '', $argument)] = $this->searchDependency(
                             $defaultConfig,
                             $value
                         );
                     } else {
-                        $index = \str_replace(['%config%', '%config.', '%'], '', $value);
-                        $arguments[\str_replace('$', '', $argument)] = empty($index)
+                        $index = str_replace(['%config%', '%config.', '%'], '', $value);
+                        $arguments[str_replace('$', '', $argument)] = empty($index)
                             ? $defaultConfig
                             : $defaultConfig[$index];
                     }
@@ -77,7 +98,7 @@ class ConfigTranslator
                 continue;
             }
 
-            if (\array_key_exists('class', $service)) {
+            if (array_key_exists('class', $service)) {
                 $config['dependencies']['invokables'][$serviceName] = $service['class'];
                 continue;
             }
@@ -89,7 +110,7 @@ class ConfigTranslator
     private function searchDependency(array $config, $value): string
     {
         foreach (self::CONFIG['dependencies'] as $item) {
-            $index = \array_search($value, $config['dependencies'][$item], true);
+            $index = array_search($value, $config['dependencies'][$item], true);
             if (false === $index) {
                 continue;
             }

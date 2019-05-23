25601840f309f8bca18c4d3d2aad53ac44a501f2
kpicaza <kpicaza.sound@gmail.com>
2019-05-23 22:31:35 +0200

    issue/#10: add first tests to config aggregator

--- src/ArgumentTranslator.php
+++ src/ArgumentTranslator.php
@@ -14,20 +14,27 @@ class ArgumentTranslator
         $arguments = [];
         /**
          * @var string $argument
-         * @var string $value
          */
         foreach ($service['arguments'] as $argument => $value) {
-            $isService = 0 === strpos($value, '@');
-            if ($isService) {
-                $arguments[str_replace('$', '', $argument)] = str_replace('@', '', $value);
-            } else {
-                $index = str_replace(['%config%', '%config.', '%'], '', $value);
-                $arguments[str_replace('$', '', $argument)] = empty($index)
-                    ? $config
-                    : $config[$index];
-            }
+            $arguments[str_replace('$', '', $argument)] = $this->getArguments($config, $value);
         }
 
         return $arguments;
+    }
+
+    private function getArguments(array $config, $value)
+    {
+        $isService = 0 === strpos($value, '@');
+        if ($isService) {
+            return str_replace('@', '', $value);
+        }
+        $index = str_replace(['%config%', '%config.', '%'], '', $value);
+        if (array_key_exists($index, $config)) {
+            return empty($index)
+                ? $config
+                : $config[$index];
+        }
+
+        return $value;
     }
 }
